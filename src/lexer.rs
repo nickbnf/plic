c5e473d73bcd5d0742085588f3d91fc3bbf8f689
Nicolas Bonnefon <nicolas@bonnefon.org>
2016-09-18 23:04:00 +0100

    Start implementing the lexer.

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,9 +1,58 @@
 // Our lexer
 
+use std;
+
+pub fn tokens<'a>( code: &'a str ) -> CodeTokens<'a> {
+    CodeTokens { iter: code.chars() }
+}
+
+#[derive(Debug)]
+pub enum Token {
+    ParenOpen,
+    ParenClose,
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Illegal,
+}
+
+pub struct CodeTokens<'a> {
+    iter: std::str::Chars<'a>,
+}
+
+impl<'a> Iterator for CodeTokens<'a> {
+    type Item = Token;
+    fn next(&mut self) -> Option<Token> {
+        let c = self.iter.next();
+        match c {
+            Some(car) => match car {
+                '(' => Some(Token::ParenOpen),
+                ')' => Some(Token::ParenClose),
+                '+' => Some(Token::Plus),
+                '-' => Some(Token::Minus),
+                '*' => Some(Token::Multiply),
+                '/' => Some(Token::Divide),
+                _ => Some(Token::Illegal)
+            },
+            None      => None
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn simple() {
-        assert!( true )
+    fn one_char_tokens() {
+        let string = "()+-*/";
+        let mut tokens = super::tokens( string );
+        assert!( matches!( tokens.next(), Some(super::Token::ParenOpen) ) );
+        assert!( matches!( tokens.next(), Some(super::Token::ParenClose) ) );
+        assert!( matches!( tokens.next(), Some(super::Token::Plus) ) );
+        assert!( matches!( tokens.next(), Some(super::Token::Minus) ) );
+        assert!( matches!( tokens.next(), Some(super::Token::Multiply) ) );
+        assert!( matches!( tokens.next(), Some(super::Token::Divide) ) );
+        assert!( matches!( tokens.next(), None ) );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use] extern crate matches;
+
 extern crate rustyline;
 
 mod lexer;
@@ -18,30 +20,12 @@ fn repl() {
 }
 
 fn evaluate_line( line: String ) {
-    let mut tokens = code_tokens( &line );
+    let mut tokens = lexer::tokens( &line );
     loop {
         match tokens.next() {
-            Some(token) => println!( "Token: {}", token ),
+            Some(token) => println!( "Token: {:?}", token ),
             None        => return(),
         }
     }
 }
 
-fn code_tokens<'a>( code: &'a String ) -> PelicCodeTokens<'a> {
-    PelicCodeTokens { iter: code.chars() }
-}
-
-struct PelicCodeTokens<'a> {
-    iter: std::str::Chars<'a>,
-}
-
-impl<'a> Iterator for PelicCodeTokens<'a> {
-    type Item = String;
-    fn next(&mut self) -> Option<String> {
-        let c = self.iter.next();
-        match c {
-            Some(car) => Some(car.to_string()),
-            None      => None
-        }
-    }
-}
