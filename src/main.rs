2efc8f67d5708ad28b54e026e5382068b2298765
Nicolas Bonnefon <nicolas@bonnefon.org>
2016-09-29 13:52:58 +0100

    Add command line processing.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
 #[macro_use] extern crate matches;
 
 extern crate rustyline;
+extern crate clap;
+
+use clap::{Arg, App, SubCommand};
 
 mod lexer;
 mod eval;
 
 fn main() {
-    repl();
+    let matches = App::new("plic")
+        .version("0.1.0")
+        .author("Nicolas Bonnefon <nicolas@bonnefon.org>")
+        .about("A tiny LISP interpreter.")
+        .args_from_usage(
+            "-e, --eval=[EXPRESSION] 'Evaluate the passed expression'
+             -v...                   'Sets the level of verbosity'
+             [INPUT]                 'Evaluate the content of this file'")
+        .get_matches();
+
+    if let Some( eval ) = matches.value_of("eval") {
+        println!("Using expression: {}", eval);
+        match eval::evaluate_line( eval.to_string() ) {
+            Ok(n) => println!("{}", n),
+            Err(e) => println!("Error: {}", e),
+        }
+    }
+    else if let Some( file ) = matches.value_of("INPUT") {
+        println!("Using input file: {}", matches.value_of("INPUT").unwrap());
+    }
+    else {
+        repl();
+    }
 }
 
 fn repl() {
